c54357ace530a21193b0b01275f8f6e3eb09ad94
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-06-19 14:47:03 -0400

    Completed 1: a,b,c, and d and partial documentation

--- COMP123-s2016-Assignment3/Planets.cs
+++ COMP123-s2016-Assignment3/Planets.cs
@@ -5,8 +5,16 @@ using System.Text;
 
 namespace COMP123_s2016_Assignment3
 {
+    /** <summary>
+     * This abstract class defines planets.
+     * </summary>
+     * 
+     * @class Planets
+     */
     public abstract class Planets
     {
+        // PRIVATE INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++++++++++++
+
         private double _diameter;
         private double _mass;
         private int _moonCount;
@@ -15,93 +23,158 @@ namespace COMP123_s2016_Assignment3
         private int _ringCount;
         private double _rotationPeriod;
 
+        // PUBLIC PROPERTIES +++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**<summary>
+        * This is a property for our _diameter field.
+        * </summary>
+        *
+        * @property Diameters
+        */
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+
+        }
+
+        /**<summary>
+        * This is a property for our _mass field.
+        * </summary>
+        *
+        * @property Mass
+        */
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+
+        }
+
+        /**<summary>
+        * This is a property for our _moonCount field.
+        * </summary>
+        *
+        * @property MoonCount
+        */
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+
+        }
+
+        /**<summary>
+        * This is a property for our _name field.
+        * </summary>
+        *
+        * @property Name
+        */
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+
+        }
+
+        /**<summary>
+        * This is a property for our _orbitalPeriod field.
+        * </summary>
+        *
+        * @property Orbital Period
+        */
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+
+        /**<summary>
+        * This is a property for our _ringCount field.
+        * </summary>
+        *
+        * @property RingCount
+        */
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+
+        /**<summary>
+        * This is a property for our _rotationPeriod field.
+        * </summary>
+        *
+        * @property RotationalPeriod
+        */
+        public double RotationalPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
+        // CONSTRUCTORS ++++++++++++++++++++++++++++++++++++++++++++
+
+        /** <summary>
+        * This constructor takes name, diameter, and mass as parameters and passes them to the PRIVATE INSTANCE VARIABLES.
+        * </summary>
+        *
+        * @constructor Planets
+        * @params {string} name
+        * @params {double} diameter
+        * @params {double} mass
+        */
         public Planets(string name, double diameter, double mass)
         {
             throw new System.NotImplementedException();
         }
 
-        public double Diameter
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
+        // PUBLIC METHODS
 
-            set
-            {
-            }
+        /** <summary>
+        * This method ToString outputs Name, Diameter, and Mass to the console
+        * </summary>
+        *
+        * @method ToString
+        * returns {string}
+        */
+        public override void ToString()
+        {
+            throw new System.NotImplementedException();
         }
 
-        public double Mass
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
-        }
-
-        public int MoonCount
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
-        }
-
-        public string Name
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
-        }
-
-        public double OrbitalPeriod
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
-        }
-
-        public int RingCount
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
-        }
-
-        public double RotationalPeriod
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-
-            set
-            {
-            }
-        }
     }
 }
--- COMP123-s2016-Assignment3/Program.cs
+++ COMP123-s2016-Assignment3/Program.cs
@@ -3,7 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/**
+ * @Author: Meer Zaheen Nazmul
+ * @StudentNumber: 300522487
+ * @DateCreated: June, 19,2016
+ * @Description: Build and implement Planet classes and Interfaces
+ * Version #: 0.0.1
+ */
 
+/**
+ ********* REVISION *********
+ * Initial Commit- Created Planet Diagram
+ * Date: June 19th, 2016
+ */
 namespace COMP123_s2016_Assignment3
 {
     class Program
