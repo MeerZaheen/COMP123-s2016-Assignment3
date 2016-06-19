32511d3178530a2bcd6d0e586a6ad40942fed90c
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-06-19 14:47:58 -0400

    updated revision on program.cs

--- COMP123-s2016-Assignment3/Program.cs
+++ COMP123-s2016-Assignment3/Program.cs
@@ -8,12 +8,14 @@ using System.Threading.Tasks;
  * @StudentNumber: 300522487
  * @DateCreated: June, 19,2016
  * @Description: Build and implement Planet classes and Interfaces
- * Version #: 0.0.1
+ * Version #: 0.0.1.1
  */
 
 /**
  ********* REVISION *********
  * Initial Commit- Created Planet Diagram
+ * Date: June 19th, 2016
+ * Completed 1: a,b,c, and d and partial documentation
  * Date: June 19th, 2016
  */
 namespace COMP123_s2016_Assignment3
